62c2cd44b23251a0da1eae3741e30a66cdda1585
Lashawn Butler <lashawnbutler50@gmail.com>
2024-02-12 19:45:46 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(location){
+if (location > 42) {
+    return location - 42
+}
+if (location < 42){
+    return 42 - location
+}
+}
+function distanceFromHqInFeet(location){
+    if (location > 42) {
+        return (location - 42) * 264
+    }
+    if (location < 42){
+        return (42 - location) * 264
+    }
+}
+function distanceTravelledInFeet(pickup, dropoff){
+    if (pickup < dropoff) {
+        return (dropoff - pickup) * 264
+    }
+    if (pickup > dropoff){
+        return (pickup - dropoff) * 264
+    }
+}
+function calculatesFarePrice(beginning, end){
+    const distance = distanceTravelledInFeet(beginning, end)
+    if (distance < 400){
+        return 0
+    }
+    if (distance >= 400 && distance <= 2000){
+        return (distance - 400) * .02
+    }
+    if (distance > 2500){
+        return ('cannot travel that far')
+    }
+    if (distance > 2000){
+        return 25
+    }
+}
